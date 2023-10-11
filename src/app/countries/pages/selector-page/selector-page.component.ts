515aba837404d0ec8d3a497dc6c83820699a45c1
Ezequiel Lino <sonisombra@gmail.com>
2023-10-11 10:44:03 -0600

    estructura de directorios

---
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './selector-page.component.html',
+  styles: [
+  ]
+})
+export class SelectorPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
