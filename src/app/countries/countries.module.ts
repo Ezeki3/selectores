f156daaf380d151975a5f63b0627cd0409929dbe
Ezequiel Lino <sonisombra@gmail.com>
2023-10-11 10:58:46 -0600

    lazy load

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { CountriesRoutingModule } from './countries-routing.module';
 
 
 
@@ -10,6 +11,7 @@ import { SelectorPageComponent } from './pages/selector-page/selector-page.compo
   ],
   imports: [
     CommonModule,
+    CountriesRoutingModule,
   ],
   exports:[ ]
 })
