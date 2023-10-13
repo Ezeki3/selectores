90b6306a921717be483e87e30c1bd6e3cf0142a2
Ezequiel Lino <sonisombra@gmail.com>
2023-10-13 10:58:36 -0600

    selector de regiones

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interfaces';
 
 @Component({
   templateUrl: './selector-page.component.html',
@@ -16,9 +18,14 @@ export class SelectorPageComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private countriesServices: CountriesService,
   ) { }
 
   ngOnInit(): void {
+  }
+
+  get regions(): Region[] {
+    return this.countriesServices.regions;
   }
 
 }
