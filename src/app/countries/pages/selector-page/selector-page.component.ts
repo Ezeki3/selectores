d11bf3106b5c0c903e0564a86c5e0454201c5bf6
Ezequiel Lino <sonisombra@gmail.com>
2023-10-16 10:22:25 -0600

    obtenemos paises por region

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
+import { switchMap } from 'rxjs';
 
 @Component({
   templateUrl: './selector-page.component.html',
@@ -31,10 +32,13 @@ export class SelectorPageComponent implements OnInit {
   }
 
   onRegionChanged():void {
-    this.myForm.get('region')!.valueChanges.subscribe( region => {
-      console.log( {region});
-      
-    })
+    this.myForm.get('region')!.valueChanges
+      .pipe(
+        switchMap( region => this.countriesServices.getCountryByRegion(region) )
+      )
+      .subscribe( region => {
+        console.log( {region});
+      })
   }
 
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Region, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
   private _regions: Region[] = [ Region.Africa, Region.Americas, Region.Asia, Region.Europe, Region.Oceania];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   get regions(): Region[]{
     return [ ...this._regions ];
   }
 
-  getCountryByRegion( region: Region ):SmallCountry[]{
+  getCountryByRegion( region: Region ):Observable<SmallCountry[]>{
+    if (!region) {
+      return of([]);
+    }
 
-    return [];
+    const url: string = `${this.baseUrl}/region/${ region}?fields=cca3,name,borders`;
+
+    return this.http.get<SmallCountry[]>(url)
+      .pipe(
+        tap( response => console.log( {response} ) )
+      );
   }
 }
